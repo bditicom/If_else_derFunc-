ba65cb3f00075e640871895ea7578b5709652536
“bditicom” <bditi.com@gmail.com>
2021-05-09 23:42:12 -0700

    If_else_derFunc

---
+++ test.py
@@ -0,0 +1,17 @@
+def abc (name):
+    if (name[0]==("A")):
+        return True
+#    Ajoy Kumar roy
+    else:
+        return False
+x=input("Enter your name")
+print(abc(x))
+
+#def greater (a,b):
+#   if (a>b):
+#        return a
+#    return b
+#x=input("return a ")
+#y=input("return b ")
+#c=greater(x,y)
+#print( str(c)+ (" is greater"))
